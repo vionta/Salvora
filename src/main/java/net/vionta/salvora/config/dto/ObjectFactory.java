7daa221ef8c5e141aca3423d3e0439f3885d60a5
Jorge Sanchez <jorge@vionta.net>
2022-11-15 00:49:45 +0100

    File config

---
+++ src/main/java/net/vionta/salvora/config/dto/ObjectFactory.java
@@ -0,0 +1,63 @@
+package net.vionta.salvora.config.dto;
+
+import jakarta.xml.bind.annotation.XmlRegistry;
+
+@XmlRegistry
+public class ObjectFactory {
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: net.vionta.salvora.mapping
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link SalvoraApplication }
+     * 
+     */
+    public SalvoraApplication createSalvoraApplication() {
+        return new SalvoraApplication();
+    }
+
+    /**
+     * Create an instance of {@link FileMapping }
+     * 
+     */
+    public FileMapping createFileMapping() {
+        return new FileMapping();
+    }
+
+    /**
+     * Create an instance of {@link Transformation }
+     * 
+     */
+    public Transformation createTransformation() {
+        return new Transformation();
+    }
+
+    /**
+     * Create an instance of {@link TransformationStep }
+     * 
+     */
+    public TransformationStep createTransformationStep() {
+        return new TransformationStep();
+    }
+
+    /**
+     * Create an instance of {@link PathParameter }
+     * 
+     */
+    public PathParameter createPathParameter() {
+        return new PathParameter();
+    }
+
+    /**
+     * Create an instance of {@link RequestParameter }
+     * 
+     */
+    public RequestParameter createRequestParameter() {
+        return new RequestParameter();
+    }
+
+}
