3b0882232aeee591af7fcb9f85802c2b322b1f92
Jorge Sanchez <jorge@vionta.net>
2022-11-15 00:49:45 +0100

    File config

---
+++ src/main/java/net/vionta/salvora/config/dto/PathParameter.java
@@ -0,0 +1,88 @@
+package net.vionta.salvora.config.dto;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlAttribute;
+import jakarta.xml.bind.annotation.XmlType;
+
+
+/**
+ * 
+ * 	Parameter element, used to map request url path parts to xslt parameters. 
+ *       
+ * 
+ * <p>Clase Java para path-parameter complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType name="path-parameter"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;attribute name="request-key" type="{http://www.w3.org/2001/XMLSchema}string" /&gt;
+ *       &lt;attribute name="xslt-param-name" type="{http://www.w3.org/2001/XMLSchema}string" /&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "path-parameter")
+public class PathParameter {
+
+    @XmlAttribute(name = "request-key")
+    protected String requestKey;
+    @XmlAttribute(name = "xslt-param-name")
+    protected String xsltParamName;
+
+    /**
+     * Obtiene el valor de la propiedad requestKey.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getRequestKey() {
+        return requestKey;
+    }
+
+    /**
+     * Define el valor de la propiedad requestKey.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setRequestKey(String value) {
+        this.requestKey = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad xsltParamName.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getXsltParamName() {
+        return xsltParamName;
+    }
+
+    /**
+     * Define el valor de la propiedad xsltParamName.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setXsltParamName(String value) {
+        this.xsltParamName = value;
+    }
+
+}
