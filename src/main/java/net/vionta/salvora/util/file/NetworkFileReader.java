a0dbdb0c652a52c67db1a81c16dc0fe27a980ea6
Jorge Sanchez <jorge@vionta.net>
2023-01-12 00:46:33 +0100

    mapping - trigger version

--- src/main/java/net/vionta/salvora/util/file/NetworkFileReader.java
+++ src/main/java/net/vionta/salvora/util/file/NetworkFileReader.java
@@ -3,14 +3,22 @@ package net.vionta.salvora.util.file;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.vionta.xfserver.ServerImpl;
 
 /**
  * Utility class to read content from network 
  * file. 
  */
 public final class NetworkFileReader {
-	
+
+	private static Logger LOGGER = LoggerFactory.getLogger(NetworkFileReader.class);
+
 	/**
 	 * Reads a file from an http server to 
 	 * a String. 
@@ -20,14 +28,23 @@ public final class NetworkFileReader {
 	 * @throws IOException
 	 */
 	public static String  readNetworkUrlToString(URL contentUrl) throws IOException {
+		LOGGER.debug("Leyendo " + contentUrl);
 		BufferedReader in = new BufferedReader(new InputStreamReader(contentUrl.openStream()));
 	    StringBuilder content = new StringBuilder();
 		String inputLine ;
 	    while ((inputLine = in.readLine()) != null) {
-	     content.append(inputLine);
+	    	LOGGER.debug(inputLine);
+	    	content.append(inputLine);
 	    }
 	    in.close();
+	    LOGGER.debug("Devolviendo "+content.substring(1,30));
 		return content.toString();
 	}	
+	
+	public static void main(String[] args) throws MalformedURLException, IOException {
+		URL url = new URL("http://localhost:8085/issues");
+		System.out.println(url);
+		System.out.println(NetworkFileReader.readNetworkUrlToString(url));
+	}
 
 }
