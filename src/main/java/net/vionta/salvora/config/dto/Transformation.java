f9cd62b748c4837220f0f8e0a0eda5ba3f71c755
Jorge Sanchez <jorge@vionta.net>
2023-01-12 00:46:31 +0100

    mapping - trigger version

--- src/main/java/net/vionta/salvora/config/dto/Transformation.java
+++ src/main/java/net/vionta/salvora/config/dto/Transformation.java
@@ -14,7 +14,7 @@ import jakarta.xml.bind.annotation.XmlType;
 public class Transformation {
 
 	public static final String LOCAL_SOURCE_TYPE = "local_file";
-	public static final String LOCAL_NETWORK_SOURCE_TYPE = "local_network";
+	public static final String LOCAL_DIRECTORY_LISTING= "directory_listing";
 	public static final String REMOTE_NETWORK_SOURCE_TYPE = "remote_network";
 	
     @XmlAttribute(name = "name", required = true)
@@ -28,6 +28,12 @@ public class Transformation {
     @XmlAttribute(name = "url")
     protected String url;
     
+    @XmlElement(name="step", type = TransformationStep.class)
+    protected List<TransformationStep> transformationSteps = new ArrayList<TransformationStep>();
+    
+    @XmlElement(name="trigger", type = Trigger.class)
+    protected List<Trigger> triggers = new ArrayList<Trigger>();
+
     public String getPath() {
 		return path;
 	}
@@ -47,10 +53,17 @@ public class Transformation {
 	@XmlAttribute(name = "type")
     protected String type = "file";
 
-	@XmlElement(name="step", type = TransformationStep.class)
-    protected List<TransformationStep> transformationSteps = new ArrayList<TransformationStep>();
+
     
-    public String getName() {
+    public List<Trigger> getTriggers() {
+		return triggers;
+	}
+
+	public void setTriggers(List<Trigger> triggers) {
+		this.triggers = triggers;
+	}
+
+	public String getName() {
         return name;
     }
 
@@ -67,7 +80,7 @@ public class Transformation {
     }
 
     public String getBaseUrl() {
-        return baseUrl;
+    	return (baseUrl == null) ? getBasePath() : baseUrl;
     }
 
     public void setBaseUrl(String value) {
@@ -93,8 +106,9 @@ public class Transformation {
 	@Override
 	public String toString() {
 		return "Transformation [name=" + name + ", path=" + path + ", basePath=" + basePath + ", baseUrl=" + baseUrl
-				+ ", url=" + url + ", type=" + type + ", transformationSteps=" + transformationSteps + "]";
+				+ ", url=" + url + ", transformationSteps=" + transformationSteps + ", triggers=" + triggers + ", type="
+				+ type + "]";
 	}
-	
+
 	
 }
