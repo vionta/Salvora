491d999a830626cc0c64cda5468cbadc2d691e5d
Jorge Sanchez <jorge@vionta.net>
2023-01-12 00:46:29 +0100

    mapping - trigger version

---
+++ src/main/java/net/vionta/salvora/launch/ArgumentsReader.java
@@ -0,0 +1,32 @@
+package net.vionta.salvora.launch;
+
+/**
+ * Utility class to manage command line 
+ * arguments.
+ */
+public class ArgumentsReader {
+
+	/**
+	 * Look for an argument on program parameters.
+	 * @param args Program parameters
+	 * @param arg Argument name to look for
+	 * @return The argument value or null if not found.
+	 */
+	public static String findArgument(String[] args, String arg) {
+		boolean found = false;
+		String argumentValue = null ; 
+		for (int i = 0; i < args.length && !found; i++) {
+			if(args[i].lastIndexOf("-"+arg)==0) {
+				if(arg.equals("h") ) {
+					found = true;
+					argumentValue = "help";
+				} else {
+				found=true;
+				argumentValue=args[i].substring(3,args[i].length());
+				}
+			}
+		}
+		return argumentValue;
+	}
+
+}
