57a30da45f615d6210cb02f4f58d9cc603c85481
Jorge Sanchez <jorge@vionta.net>
2023-01-12 00:51:21 +0100

    n/a

---
+++ src/main/java/net/vionta/salvora/config/dto/Parameter.java
@@ -0,0 +1,33 @@
+package net.vionta.salvora.config.dto;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlAttribute;
+import jakarta.xml.bind.annotation.XmlType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "parameter")
+public class Parameter {
+
+    @XmlAttribute(name = "name")
+    protected String name;
+    @XmlAttribute(name = "value")
+    protected String value;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	@Override
+	public String toString() {
+		return "Parameter [name=" + name + ", value=" + value + "]";
+	}
+
+}
