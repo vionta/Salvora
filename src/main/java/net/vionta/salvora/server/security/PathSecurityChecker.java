ae34e3aa14146fd2a19beec3492d37b5965f6c73
Jorge Sanchez <jorge@vionta.net>
2023-02-22 00:35:17 +0100

    0.9.1

---
+++ src/main/java/net/vionta/salvora/server/security/PathSecurityChecker.java
@@ -0,0 +1,13 @@
+package net.vionta.salvora.server.security;
+
+public interface PathSecurityChecker {
+	
+	/**
+	 * Writes a file content to its path. May implement backup in a future. 
+	 * 
+	 * @param path File path. 
+	 * @param Contents File contents as UTF-8 String. 
+	 */
+	void checkPath(String path);
+
+}
