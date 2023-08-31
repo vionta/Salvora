18576b2b282fcb0c5f4e98b4f3c73575478e302e
Jorge Sanchez <jorge@vionta.net>
2023-08-31 14:24:50 +0200

    source input port and refactoring

--- src/main/java/net/vionta/salvora/util/xml/XProcRunner.java
+++ src/main/java/net/vionta/salvora/util/xml/XProcRunner.java
@@ -4,6 +4,7 @@ package net.vionta.salvora.util.xml;
 import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -19,15 +20,19 @@ import com.xml_project.morganaxproc3.XProcOutput;
 import com.xml_project.morganaxproc3.XProcPipeline;
 import com.xml_project.morganaxproc3.XProcResult;
 import com.xml_project.morganaxproc3.XProcSource;
+import com.xml_project.morganaxproc3.compiler.XProcCompilerException;
 import com.xml_project.morganaxproc3.documents.XProcDocument;
 
+import net.vionta.salvora.config.dto.TransformationParameter;
 import net.vionta.salvora.config.dto.Trigger;
+import net.vionta.salvora.server.response.error.ErrorManager;
+import nu.validator.saxtree.Document;
 
 public class XProcRunner {
 
 	static Logger LOGGER = LoggerFactory.getLogger(XProcRunner.class);
 
-	public static void run(Trigger trigger, String content, Map<String, String> parameters) {
+	public static void run(Trigger trigger, String content, List<TransformationParameter> parameters) {
 		
 		LOGGER.debug(" Running trigger "+trigger.getName());
 		/* Create a source with the pipeline */
@@ -35,27 +40,35 @@ public class XProcRunner {
 		try{
 			URI xprocUri = loadXProcFilePath(trigger.getSource());
 			XProcSource pipelineSource = new XProcSource(xprocUri);
-//			LOGGER.debug(" pipeline Source  "+xprocUri);
+			LOGGER.debug(" Pipeline Source  "+xprocUri);
 			/* Create an XProcEngine */
 			XProcEngine engine = XProcEngine.newXProc();	
 			/* Create a new compiler */
 			LOGGER.debug(" Engine "+engine);
 			XProcCompiler compiler = engine.newXProcCompiler();			
-			/* Compile the pipeline */
+			LOGGER.debug(" Compiler :"+compiler);
+			
 			XProcPipeline pipeline = compiler.compile(pipelineSource);
 			LOGGER.debug(" Pipeline "+pipeline);
 			
 			XProcInput input = new XProcInput(engine.getConfiguration());
-//			for(Parameter parameter : trigger.getParameters()) {
-//				LOGGER.debug(" Adding parameter "+parameter.getName());
-//				input.setOption(new QName(parameter.getName()), parameter.getValue());
-//			}
 			
-			for(Entry<String, String> parameter : parameters.entrySet()) {
-				LOGGER.debug(" Adding parameter "+parameter.getKey()+" -> "+parameter.getValue());
-				input.setOption(new QName(parameter.getKey()), "'"+parameter.getValue()+"'");
+//			for(Entry<String, String> parameter : parameters.entrySet()) {
+//				LOGGER.debug(" Adding parameter "+parameter.getKey()+" -> "+parameter.getValue() );
+//				input.setOption(new QName(parameter.getKey()), "'"+parameter.getValue()+"'");
+//			}
+
+			for(TransformationParameter parameter : parameters) {
+				LOGGER.debug(" Adding parameter "+parameter.getName()+" -> "+parameter.getValue() );
+				if(parameter.getInputPort()) input.addInput(parameter.getName(), new XProcSource(loadXProcFilePath(parameter.getValue())));
+				else input.setOption(new QName(parameter.getName()), "'"+parameter.getValue()+"'");
 			}
 			
+//			compiler.
+//			XProcDocument xProcSource = new XProcDocument();
+//			XProcSource pipelineSource = new XProcSource();
+//			XProcDocument pipelineSource = new XProcDocument;
+//			input.addInput("source" , xProcSource);
 			
 			/* Execute the pipeline */
 			XProcOutput output = pipeline.run(input);
@@ -74,41 +87,29 @@ public class XProcRunner {
 					LOGGER.debug("Port > "+portName);
 					XProcResult result = output.getResults(portName);
 //					//XProcResult result = output.getResults(ports[i]);
-//					//XProcResult result = output.getResults(ports[i]);
 //					/* Get the documents on this port serialized */
 //					List<XProcDocument> document = result.getDocuments();
 					for(XProcDocument document : result.getDocuments() ) {
 						LOGGER.debug(document.getStringRepresentation(true));
 					}
 				}
-//			//for (int j=0; j < document.length; j++)
-//			//LOGGER.debug(documents[j]);
 			}
-//
-//			} else {
-//	
-//			/* Examine runtime errors */
-//			LOGGER.debug(output.getErrorDocumentSerialized());
-//			}
+
 			//We don't throw errors on trigger failures. 
-//		} catch(XProcCompilerException ex) {
-//			/* Examine compilation error */
-//			LOGGER.error(" Cause: {1}", ex.getCause());
-//		} catch (IOException ex) {
-//			LOGGER.error(" A failure was detected on the trigger file management");
-//			LOGGER.error(" Cause: {1}", ex.getCause());
-//		} catch (XProcInterfaceException e) {
-//			LOGGER.error(" XProc Error: {1}", e.getCause());
+		} catch(XProcCompilerException ex) {
+			LOGGER.error(" XProc Compilation Error ", ex.getCause());
 		}	catch (Exception e) {
+			e.printStackTrace();
+			ErrorManager.logError(null, "Error ejecutando Xproc  "+trigger.getName()+ "| Causa :"+e.getMessage()+" - "+e.getCause());
 			LOGGER.error(" XProc Error: (1)", e.getCause());
 			LOGGER.error(" XProc Error: {}", e.getCause());
-			LOGGER.error(" XProc Error: "+ e.getCause());
+			LOGGER.error(" XProc Error: "+ e.getMessage());
 			e.printStackTrace();
 		}
 
 	}
 
-	private static URI loadXProcFilePath(String source) throws URISyntaxException {
+	public static URI loadXProcFilePath(String source) throws URISyntaxException {
 		if (calculateLocalFileAbsolutePath(source)==null) throw new IllegalStateException(" Trigger configuration is empty, no url was provided ");
 		URI xprocUri = new URI(calculateLocalFileAbsolutePath(source));
 		File f = new File(xprocUri);
@@ -116,7 +117,7 @@ public class XProcRunner {
 		return xprocUri;
 	}
 
-	private static String calculateLocalFileAbsolutePath(String source) {
+	public static String calculateLocalFileAbsolutePath(String source) {
 		System.out.println("Working Directory = " + System.getProperty("user.dir"));
 		String userDirectory = new File("").getAbsolutePath().replace("\\", "/");
 		System.out.println(" UD "+userDirectory);
@@ -125,12 +126,4 @@ public class XProcRunner {
 		return calculatedURL;
 	}
 
-	
-//	public static void main(String[] args) {
-//		String testUrl = "hola.xpl";
-//		String expectedResult = "file:///C:/dev/ws/salvora-ws/salvora/Salvora/hola.xpl";
-//		System.out.println(" Response " +calculateLocalFileAbsolutePath(testUrl));
-//		System.out.println(" > " +calculateLocalFileAbsolutePath(testUrl).equals(expectedResult));
-//	}
-	
 }
