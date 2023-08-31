2ff8f92032f52c6bc055e3edc6756da370852e0a
Jorge Sanchez <jorge@vionta.net>
2023-08-31 14:24:49 +0200

    source input port and refactoring

--- src/main/java/net/vionta/salvora/server/response/error/ErrorManager.java
+++ src/main/java/net/vionta/salvora/server/response/error/ErrorManager.java
@@ -26,9 +26,28 @@ public class ErrorManager {
 	 * @param message
 	 */
 	public static void notifyError(HttpServerResponse response, String message) {
-		LOGGER.warn(message);
-		response.write(message);
-		close(response, 500);
+		notifyError(response, message, 500);
 	}
+
+	/**
+	 * Simple method to print error messages to the 
+	 * response and the log thread. 
+	 * 
+	 * @param response
+	 * @param message
+	 */
+	public static void notifyError(HttpServerResponse response, String message, int errorCode) {
+		LOGGER.warn(message);
+		response.putHeader("Content-length", "0");
+		response.setStatusCode(errorCode);
+		response.write(message);
+		response.setStatusCode(errorCode);
+		close(response, errorCode);
+	}
+	
+	public static void logError(HttpServerResponse response, String message) {
+		LOGGER.warn(message);
+	}
+	
 
 }
